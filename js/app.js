fe842b0b1dc1721c439791181ff0fa76c92cf7ea
shenmadu <tashen.madu@gmail.com>
2023-12-11 23:22:58 +0530

    add report and dashboard ui

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-document.getElementById("btn-submit").onclick=function(){
-    
-}
+document.getElementById("btn-login").onclick = function () {
+    let txtUsername = document.getElementById("userName").value;
+    let txtPassword = document.getElementById("pwd").value;
+
+    if (txtUsername == "user" && txtPassword == "1234") {
+        window.location.href = 'index.html';
+        
+    } else  if (txtUsername == "user"){
+        document.getElementById("log-status").innerHTML = "Invalid password!";
+    }else{
+        document.getElementById("log-status").innerHTML = "Invalid User Name!";
+    }
+};
+
