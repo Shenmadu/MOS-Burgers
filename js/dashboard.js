1c723eb9ca37b1fcd9c09e15a5d430f7e66913cc
shenmadu <tashen.madu@gmail.com>
2023-12-12 19:20:48 +0530

    add item to menu ui

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,7 +3,7 @@ document.getElementById("btn-login").onclick = function () {
     let txtPassword = document.getElementById("pwd").value;
 
     if (txtUsername == "user" && txtPassword == "1234") {
-        window.location.href = 'index.html';
+        window.location.href = 'menu.html';
         
     } else  if (txtUsername == "user"){
         document.getElementById("log-status").innerHTML = "Invalid password!";
